9c6ae632cc405bf604ceea5caada58ce76a41127
Nour Ahmed <sim.nour.ahmed@alexu.edu.eg>
2025-04-12 16:50:10 +0200

    dart

--- lib/home/home_screen/home_page.dart
+++ lib/home/home_screen/home_page.dart
@@ -1,4 +1,6 @@
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_for_beginners/profile/profile_page/profile_page.dart';
 import '../../first_screen.dart';
@@ -11,7 +13,8 @@ class MyHomePage extends StatelessWidget {
 
   final String ? body;
 
-  const MyHomePage({ this.title,  this.body, super.key});
+
+   MyHomePage({    this.title,  this.body, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -41,13 +44,22 @@ class MyHomePage extends StatelessWidget {
             Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(textAlign: TextAlign.justify, body ?? "A tree is a tall plant with a woody stem, branches, and leaves. It plays a vital role in nature by producing oxygen, providing shade, and offering a home to many animals. Trees absorb carbon dioxide and help reduce air pollution. They come in many types, like oak, pine, and mango. Trees also prevent soil erosion and are important for the water cycle. People use trees for wood, paper, and fruit. In cities and forests, trees add beauty and support life. Planting and protecting trees is essential for a healthy environment and a better future for all living things.")),
-            Row(
+               Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 MySeason(url: "assets/tree_spring.jpg", text: "Spring"),
                 MySeason(url: "assets/tree_fall.jpg", text: "Fall"),
               ],
-            )
+            ) ,
+
+
+
+
+
+
+
+
+
           ],
         ),
       ),
